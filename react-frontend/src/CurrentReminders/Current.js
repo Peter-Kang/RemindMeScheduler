9ce407331a62e990703636c98d862396f9921c6a
Peter Kang <pxk7062@rit.edu>
2023-02-14 20:12:18 -0500

    Added some structure

---
+++ react-frontend/src/CurrentReminders/Current.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Current.css';
+
+const Current = ()=>
+{
+    return (
+        <body>
+            <a><font color="white">hello this is current</font></a>
+        </body>
+    );
+}
+
+export default Current;
