2139b0970e4fb7c3f1d991467c5de95996b64b0f
Peter Kang <pxk7062@rit.edu>
2023-02-16 15:13:30 -0500

    updated some spcing

--- react-frontend/src/CurrentReminders/Current.js
+++ react-frontend/src/CurrentReminders/Current.js
@@ -10,18 +10,19 @@ const Current = ()=>
             <br/>
             <Row className="d-flex align-items-center">
                 <Col md={12}>
-                <Alert variant="success">
-                    <Row>
-                        <Col md={8}> 
-                            This is a message!
-                        </Col>
-                        <Col md={4} className="d-flex justify-content-end">
-                            <Button variant="primary" className="mx-2">Reset</Button>
-                        </Col>
-                    </Row>
+                    <Alert variant="success">
+                        <Row>
+                            <Col md={8}> 
+                                This is a message!
+                            </Col>
+                            <Col md={4} className="d-flex justify-content-end">
+                                <Button variant="primary" className="mx-2">Reset</Button>
+                            </Col>
+                        </Row>
                     </Alert>
                 </Col>
             </Row>
+            
         </Container>
     );
 }
