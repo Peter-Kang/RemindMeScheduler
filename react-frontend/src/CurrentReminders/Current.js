aba2031ebe719f0829102eb25b3aa5474f368eac
Peter Kang <pxk7062@rit.edu>
2023-02-16 15:10:26 -0500

    inital current page alert and close button is good enough

--- react-frontend/src/CurrentReminders/Current.js
+++ react-frontend/src/CurrentReminders/Current.js
@@ -1,12 +1,28 @@
 import React from "react";
 import './Current.css';
+import {Container, Row, Col, Alert} from "react-bootstrap"
+import Button from 'react-bootstrap/Button';
 
 const Current = ()=>
 {
     return (
-        <body>
-            <a><font color="white">hello this is current</font></a>
-        </body>
+        <Container>
+            <br/>
+            <Row className="d-flex align-items-center">
+                <Col md={12}>
+                <Alert variant="success">
+                    <Row>
+                        <Col md={8}> 
+                            This is a message!
+                        </Col>
+                        <Col md={4} className="d-flex justify-content-end">
+                            <Button variant="primary" className="mx-2">Reset</Button>
+                        </Col>
+                    </Row>
+                    </Alert>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
