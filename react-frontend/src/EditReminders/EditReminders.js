4575fcf6d74c39337f501eaf63693c955e2af5ec
Peter Kang <pxk7062@rit.edu>
2023-02-24 12:57:18 -0500

    Created  add section in the edit UI, not hooked up

--- react-frontend/src/EditReminders/EditReminders.js
+++ react-frontend/src/EditReminders/EditReminders.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './EditReminders.css';
+//bootstrap
 import {Container, Row, Col, Alert} from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
+//mui
+import dayjs from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
 
 const EditReminders = ()=>
 {
+    const [value, setValue] = React.useState(dayjs('2022-04-07'));
     return (
         <Container>
             <br/>
@@ -15,9 +23,26 @@ const EditReminders = ()=>
                 </Col>
             </Row>
             <br/>
-            <Row className="d-flex align-items-center">
-                <Col md={12}>
+            <h2><font color='white'>Add</font></h2>
+            <Row md={12}className="d-flex align-items-center">
+                <Col col-sm><font color='white'>Message: </font><input></input></Col>
+                <Col col-sm><font color='white'>Frequency in hours: </font><input></input></Col>
+                <Col col-sm>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateTimePicker
+                          renderInput={(props) => 
+                            <TextField {...props} 
+                                className="createStartDatePicker"
+                                sx={{svg: { color: 'white' },
+                                    input: { color: 'white' }
+                                }} 
+                            />}
+                          label="Starting DateTime:"
+                          value={value}
+                          onChange={(newValue) => {setValue(newValue);}}/>
+                    </LocalizationProvider>
                 </Col>
+                <Col col-sm>  <Button variant="btn btn-secondary" href={"/"} >Add</Button> </Col>
             </Row>
         </Container>
     );
