695601f068a04a08405d7394ca7df15ab297b772
Peter-Kang <pkang93@gmail.com>
2023-03-09 15:21:31 -0500

    Added Cors Oak

--- deno-backend/Webserver.ts
+++ deno-backend/Webserver.ts
@@ -1,43 +1,29 @@
-// Start listening on port 8080 of localhost.
-const server = Deno.listen({ port: 8080 });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8000/`);
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-// Connections to the server will be yielded up as an async iterable.
-for await (const conn of server) {
-  // In order to not be blocking, we need to handle each connection individually
-  // without awaiting the function
-    serveHttp(conn);
-}
+const app = new Application();
+const router = new Router();
 
-async function serveHttp(conn: Deno.Conn) {
-  // This "upgrades" a network connection into an HTTP connection.
-  const httpConn = Deno.serveHttp(conn);
-  // Each request sent over the HTTP connection will be yielded as an async
-  // iterator from the HTTP connection.
-  for await (const requestEvent of httpConn) {
-    // The native HTTP server uses the web standard `Request` and `Response`
-    // objects.
+app.use(oakCors());
+
+app.use(async (requestEvent) => {
+    const result = requestEvent.request.body(); 
+    //requestEvent.response.body = await result.value;
     let body = '';
+    body = `Alive! Your user-agent is:\n\n${
+      requestEvent.request.headers.get("user-agent") ?? "Unknown"
+    }`;
     if( requestEvent.request.method === 'GET' )
     {
       const url = new URL(requestEvent.request.url, `http://${requestEvent.request.headers.get('host')}`);
       if( url.pathname === '/HealthCheck' )
       {
-        body = 'true';
-      }
-      else
-      {
-        body = `Alive! Your user-agent is:\n\n${
-          requestEvent.request.headers.get("user-agent") ?? "Unknown"
-        }`;
+        body = 'Alive';
       }
     }
-    // The requestEvent's `.respondWith()` method is how we send the response
-      // back to the client.
-      requestEvent.respondWith(
-        new Response(body, {
-          status: 200,
-        }),
-      );
-  }
-}
+    requestEvent.response.body = await body;
+  });
+
+app.use(router.routes());
+await app.listen({ port: 8080 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8000/`);
