67a84dfe2f3d4ea57c829e1a52c1a681bf9fd75a
Peter Kang <pxk7062@rit.edu>
2023-03-29 16:29:02 -0400

    CreatedToDo Api call sucessfully sends and recives a message from the server

--- react-frontend/src/hooks/useTodos.js
+++ react-frontend/src/hooks/useTodos.js
@@ -1,7 +1,32 @@
 import {useState, useEffect} from "react";
+import API from '../utils/api.js'
 
 const fakeTodoDB = [{id: 0, message: 'Fake Todo', startDateTime: '01/05/2023'}]
 
+//api calls
+export const createTodoAPICall = async (params) => {
+	console.log('createTodoAPICall');
+	const { data } = await API.post
+	('/CreateToDo',
+		params
+	).then
+	(
+		function(response)
+		{
+			console.log(response) 
+		}
+	).catch
+	(
+		function(error)
+		{
+			console.log(error)
+		}
+	);
+	console.log(data);
+	return data;
+  };
+
+//hooks
 export const fetchTodos = () => {
   console.log('fetch todos')
 	return [...fakeTodoDB]
@@ -25,7 +50,7 @@ export const useTodos = () => {
 
 		// should actually be a network request to add new todo, is push for fake purposes
 		fakeTodoDB.push({id: 1, message: 'Fake Todo 2', startDateTime: '03/24/2023' })
-		
+		createTodoAPICall(newTodoData);
 		const newTodos = await fetchTodos()
 		setTodos(newTodos)
 	}
