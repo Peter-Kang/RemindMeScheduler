5894312797cd9cae69eb4a91072eb3b382dfb63f
Peter Kang <pxk7062@rit.edu>
2023-04-26 23:39:34 -0400

    FOUND THE ISSUE. Changed localhost to the ip of gateway for Docker containers on the default Docker network

--- deno-backend/src/utils/connectToDB.ts
+++ deno-backend/src/utils/connectToDB.ts
@@ -1,8 +1,21 @@
+// import { MongoClient } from '../../deps.ts';
+
+// const conn = async () => {
+//   const client = new MongoClient();
+//   const srv = `mongodb://admin:password123@mongo:6000/deno_mongodb?authMechanism=SCRAM-SHA-1&retryWrites=true&w=majority`
+//   await client.connect(srv);
+//   return client;
+// }
+
+// export const db = await conn();
+
+
 import { MongoClient } from '../../deps.ts';
 import config from '../../config/default.ts';
-
 const client: MongoClient = new MongoClient();
-
-const db = await client.connect('mongodb://admin:password123@mongo:6000/deno_mongodb');
-
+const db = await client.connect('mongodb://172.17.0.1:6000/deno_mongodb');
+console.log("Worked");
 export { db };
+
+//Found the solution here: changed ip from localhost to 172.17.0.1
+//https://github.com/denoland/deno/issues/7686#issuecomment-1380142892
