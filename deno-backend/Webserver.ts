e29a1cb60be7bbcd9bf4b8c2e882b8f38a94f6ab
Peter-Kang <pkang93@gmail.com>
2023-06-01 10:53:27 -0400

    Cleaned up main driver for deno

--- deno-backend/Webserver.ts
+++ deno-backend/Webserver.ts
@@ -22,39 +22,6 @@ router.get<string>('/HealthCheck', (ctx: RouterContext<string>) => {
   };
 });
 
-/*
-app.use(async (ctx) => {
-  //ctx.response.body = await result.value;
-  let body = '';
-  body = `Alive!! Your user-agent is:\n\n${ctx.request.headers.get("user-agent") ?? "Unknown"
-    }`;
-  const requestMethod = ctx.request.method;
-  const url = new URL(ctx.request.url, `http://${ctx.request.headers.get('host')}`);
-  switch (requestMethod) {
-    case "GET":
-      if (url.pathname === '/HealthCheck') {
-        body = 'Alive!';
-      }
-      if (url.pathname === '/GetAllToDo') {
-        const scaffoldResponse = [{ id: 0, message: 'Fake ToDo', startDateTime: '01/05/2023' },{ id: 2, message: 'Fake ToDo2', startDateTime: '02/05/2021' }];
-        const json = JSON.stringify(scaffoldResponse);
-        console.log(json);
-        body = json;
-      }
-      break;
-    case "POST":
-      if (url.pathname === '/CreateToDo') {
-        const jsonResultValue = await ctx.request.body({ type: "json" }).value;
-        console.log(jsonResultValue);
-      }
-      break;
-    default:
-      break;
-  }
-  ctx.response.body = await body;
-});
-*/
-
 //evoke routers
 appRouter.init(app)
 app.use(router.routes()); // Implement our router
