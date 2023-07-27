0c2a35439903368590f26a9cb5200ca3335e682a
Peter Kang <pxk7062@rit.edu>
2023-07-27 12:41:07 -0400

    refreshed some ws

--- deno-backend/Webserver.ts
+++ deno-backend/Webserver.ts
@@ -1,65 +1,54 @@
-import { Application,oakCors,Router, RouterContext } from './deps.ts';
-import appRouter from './src/routes/index.ts';
+import {
+  Application,
+  Context,
+  oakCors,
+  Router,
+  RouterContext,
+} from "./deps.ts";
+import appRouter from "./src/routes/index.ts";
 
 const app = new Application();
 const router = new Router(); //Create the new router
 
-
 app.use(oakCors());
 
-// Middleware Logger
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
-});
-
 //Health Check API
-router.get<string>('/HealthCheck', (ctx: RouterContext<string>) => {
+router.get<string>("/HealthCheck", (ctx: RouterContext<string>) => {
   ctx.response.body = {
     status: "success",
-    message: "Alive!"
+    message: "Alive!",
   };
 });
 /*
-router.get<string>("/wss", (ctx) => {
-  if (!ctx.isUpgradable) {
-    ctx.throw(501);
-  }
-  const ws = ctx.upgrade();
+const webSocketInitReminderHandlers = (ws: WebSocket) => {
   ws.onopen = () => {
     console.log("Connected to client");
     ws.send("Hello from server!");
-  };
-  ws.onmessage = (m) => {
-    console.log("Got message from client: ", m.data);
-    ws.send(m.data as string);
-    ws.close();
-  };
-  ws.onclose = () => console.log("Disconnected from client");
-});
-*/
-
-const webSocketReminderHandler = (
-  ws: WebSocket) => 
-{
-  ws.onopen = () => {
-    console.log("Connected to client");
-    ws.send("Hello from server!");
-  };
-  ws.onmessage = (m) => {
-    console.log("Got message from client: ", m.data);
-    ws.send(m.data as string);
-    ws.close();
   };
   ws.onclose = () => console.log("Disconnected from client");
 };
-router.get<string>("/wss", async ctx=>{ 
-  const sock = await ctx.upgrade();
-  webSocketReminderHandler(sock);});
 
+const webSocket:WebSocket = router.get<string>(
+  "/wss",
+  async (ctx: RouterContext<string>) => {
+    const sock = await ctx.upgrade();
+    webSocketInitReminderHandlers(sock);
+    return sock;
+  }
+);
+
+async function reqHandler(req: Request) {
+  if (req.headers.get("upgrade") != "websocket") {
+    return new Response(null, { status: 501 });
+  }
+  const { socket: ws, response } = Deno.upgradeWebSocket(req);
+  return response;
+}
+
+setInterval(() => {}, 60000);
+*/
 //evoke routers
-appRouter.init(app)
+appRouter.init(app);
 app.use(router.routes()); // Implement our router
 app.use(router.allowedMethods()); // Allow router HTTP methods
 
--- react-frontend/src/hooks/useTodos.js
+++ react-frontend/src/hooks/useTodos.js
@@ -1,6 +1,5 @@
-
-import { useEffect, useState } from 'react';
-import API from '../utils/api.js'
+import { useEffect, useState } from "react";
+import API from "../utils/api.js";
 import useRequest from "./useRequest.js";
 
 //api calls
@@ -8,83 +7,100 @@ import useRequest from "./useRequest.js";
 
 //move to apis
 const createToDoAPICall = async (params) => {
-	console.log('createTodoAPICall');
-	const filteredParams = { "message": params.messageValueInput,
-		"context":"ToDo",
-		"startDateTime":String(params.startingDateValue),
-		"frequencyInHours":String(params.frequencyValueInputInt) };
-	console.log(filteredParams);
-	const { data } = await API.post('/ToDo/CreateToDo',filteredParams);
+  console.log("createTodoAPICall");
+  const filteredParams = {
+    message: params.messageValueInput,
+    context: "ToDo",
+    startDateTime: String(params.startingDateValue),
+    frequencyInHours: String(params.frequencyValueInputInt),
+  };
+  console.log(filteredParams);
+  const { data } = await API.post("/ToDo/CreateToDo", filteredParams);
 
-	return data;
+  return data;
 };
 
 const updateToDoAPICall = async (params) => {
-	console.log('updateToDoAPICall');
-	const filteredParams = { 
-		"context":"ToDo",
-		"id":params.id,
-		"message": params.messageValueInput,
-		"startDateTime":String(params.startDateTimeValueInput),
-		"frequencyInHours":String(params.frequencyInHoursValueInput),
-		"status":params.statusValueInput };
+  console.log("updateToDoAPICall");
+  const filteredParams = {
+    context: "ToDo",
+    id: params.id,
+    message: params.messageValueInput,
+    startDateTime: String(params.startDateTimeValueInput),
+    frequencyInHours: String(params.frequencyInHoursValueInput),
+    status: params.statusValueInput,
+  };
 
-	console.log(filteredParams);
-	const uri = '/ToDo/'+params.id;
-	const { data } = await API.patch(uri,filteredParams);
-	return data;
-}
+  console.log(filteredParams);
+  const uri = "/ToDo/" + params.id;
+  const { data } = await API.patch(uri, filteredParams);
+  return data;
+};
 
-const deleteToDoAPICall = async (params) =>
-{
-	console.log ('deleteToDoAPICall');
-	const filteredParams = {"context":"ToDo"};
-	const uri ='/ToDo/'+params.id;
-	console.log(uri);
-	await API.delete(uri,filteredParams);
-}
+const deleteToDoAPICall = async (params) => {
+  console.log("deleteToDoAPICall");
+  const filteredParams = { context: "ToDo" };
+  const uri = "/ToDo/" + params.id;
+  console.log(uri);
+  await API.delete(uri, filteredParams);
+};
 
 //hooks
 const useGetAllTodos = () => {
-	//List of messages current state
-	return useRequest('/ToDo/GetAllToDo');
+  //List of messages current state
+  return useRequest("/ToDo/GetAllToDo");
 };
 
 //interface for todos
 export const useTodos = () => {
-	const [todos, setToDos] = useState([]);
+  const [todos, setToDos] = useState([]);
 
-	const getAllTodoAPICall = async () => {
-		const { data }  = await API.get('/ToDo/GetAllToDo');
-		let reminderResultArray = [];
-		if(data?.data?.todos)
-		{
-			reminderResultArray = data.data.todos;
-		}
-		setToDos(reminderResultArray);
-	};
+  const getAllTodoAPICall = async () => {
+    const { data } = await API.get("/ToDo/GetAllToDo");
+    let reminderResultArray = [];
+    if (data?.data?.todos) {
+      reminderResultArray = data.data.todos;
+    }
+    setToDos(reminderResultArray);
+  };
 
-	useEffect(()=> {
-		getAllTodoAPICall();
-	},[])
+  useEffect(() => {
+    getAllTodoAPICall();
+  }, []);
 
-	const createToDo = async (params) =>
-	{
-		await createToDoAPICall(params);
-		getAllTodoAPICall();
-	}
+  const createToDo = async (params) => {
+    await createToDoAPICall(params);
+    getAllTodoAPICall();
+  };
 
-	const updateToDo = async (params) =>
-	{
-		await updateToDoAPICall(params);
-		getAllTodoAPICall();
-	}
+  const updateToDo = async (params) => {
+    await updateToDoAPICall(params);
+    getAllTodoAPICall();
+  };
 
-	const deleteToDo = async (params) =>
-	{
-		await deleteToDoAPICall(params);
-		getAllTodoAPICall();
-	}
+  const deleteToDo = async (params) => {
+    await deleteToDoAPICall(params);
+    getAllTodoAPICall();
+  };
+  return { todos, createToDo, updateToDo, deleteToDo };
+};
 
-	return {todos, createToDo, updateToDo, deleteToDo};
-}
+export const useActiveTodos = () => {
+  const [activeTodos, setActiveTodos] = useState([]);
+  const ws = new WebSocket("ws://localhost:8000/wss");
+
+  const initWebSockets = async () => {
+    ws.onopen = () => console.log("Connecting to server");
+    ws.onmessage = (m) => {
+      // update list of reminders
+      console.log(m.data);
+      setActiveTodos(m.data);
+    };
+    ws.onclose = () => console.log("Disconnected from server");
+  };
+  initWebSockets();
+
+  useEffect(() => {}, [activeTodos]);
+
+  return { activeTodos };
+};
